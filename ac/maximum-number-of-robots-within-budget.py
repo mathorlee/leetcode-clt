0f7c6bfb4718c6d4b76b87e4424e547f854b22ff
songsong.li <songsong.li@pinlandata.com>
2022-12-25 15:00:25 +0800

    maximum-number-of-robots-within-budget ac, runtime_percentile: 70.13979999999992, memory_percentile: 72.95779999999999

---
+++ ac/maximum-number-of-robots-within-budget.py
@@ -0,0 +1,124 @@
+def read_input():
+    import os
+    path = os.path.join(os.path.dirname(__file__), "input.txt")
+    with open(path, "r") as f:
+        return [eval(_) for _ in f.read().split("\n")]
+
+def binary_of_int(i: int, width: int) -> str:
+    if i >= 0:
+        return f"%0{width}d" % int(bin(i)[2:])
+    
+    # if i < 0, binary(i) = 反码(binary(-i - 1))
+    s = ""
+    for _ in binary_of_int(-i - 1, width):
+        if _ == "1":
+            s += "0"
+        else:
+            s += "1"
+    return s
+
+def mask_to_bits(mask: int):
+    import math
+    while mask:
+        i = mask & -mask
+        yield int(math.log2(i))
+        mask = mask ^ i
+    yield from ()
+
+def get_primes(N):
+    # N=round(N) + 1
+    N = N + 1
+    is_prime = [True] * N
+    for i in range(2, N):
+        if is_prime[i]:
+            j = i * 2
+            while j < N:
+                is_prime[j] = False
+                j += i
+    return [_ for _ in range(2, N) if is_prime[_]]
+
+def k_array(s):
+    ret = [0] * len(s)
+    for i in range(1, len(s)):
+        j = ret[i - 1]
+        while j > 0 and s[i] != s[j]:
+            j = ret[j - 1]
+        if s[i] == s[j]:
+            j += 1
+        ret[i] = j
+    return ret
+
+
+class SectionCut(object):
+    def calculate_cuts2(self, vals: list[int], edges: list[list[int]]) -> int:
+        """
+        根节点->子节点的路径上的所有节点的val是非降序的，找到所有这样的子节点，按照子树和val分组
+        """
+        from collections import defaultdict
+
+        edge_d = defaultdict(list)
+        for a, b in edges:
+            edge_d[a].append(b)
+            edge_d[b].append(a)
+        visisted = set()
+
+        self.res = 0
+        def dfs(root: int, depth):
+            """return {val: count} Count(_.val for _ in root if root -> _ is non descending on val)"""
+            print(" " * depth * 4 + f"({vals[root]})")
+            visisted.add(root)
+            d = defaultdict(int)
+            for i, child in enumerate(edge_d[root]):
+                if child not in visisted:
+                    child_d = dfs(child, depth + 1)
+                    if vals[child] <= vals[root]:
+                        for val, count in child_d.items():
+                            d[val, i] += count
+
+            new_d = defaultdict(list)
+            for (val, _), count in d.items():
+                new_d[val].append(count)
+            for val, counts in new_d.items():
+                for i in range(len(counts)):
+                    for j in range(i + 1, len(counts)):
+                        self.res += counts[i] * counts[j]
+                if val == vals[root]:
+                    self.res += sum(counts)
+
+            new_d = {val: sum(counts) for val, counts in new_d.items()}
+            new_d[vals[root]] = new_d.get(vals[root], 0) + 1
+            return new_d
+
+        dfs(0, 0)
+        return self.res + len(vals)
+
+    def calculate_cuts(self, chargeTimes: list[int], runningCosts: list[int], budget: int) -> int:
+        # begin
+        """
+        sorteddict, 以k为end的合法子序列的最大长度
+        """
+        from sortedcontainers import SortedList
+        
+        res = 0
+        n = len(chargeTimes)
+        charges = SortedList()
+        charge_cost, running_cost = 0, 0
+
+        for i in range(n):
+            charges.add(chargeTimes[i])
+            charge_cost = charges[-1]
+            running_cost += runningCosts[i]
+            while charges and charge_cost + len(charges) * running_cost > budget:
+                j = i - len(charges) + 1
+                charges.remove(chargeTimes[j])
+                charge_cost = charges[-1] if charges else 0
+                running_cost -= runningCosts[j]
+            res = max(res, len(charges))
+        return res
+        # end
+
+f = SectionCut().calculate_cuts
+# input = read_input()
+# print(f(*input))  # [1,1,2,2,2,2,2,2,2,1]
+print(f(chargeTimes=[3,6,1,3,4], runningCosts=[2,1,3,4,5], budget=25))  # 3
+# print(f(chargeTimes=[11,12,19], runningCosts=[10,8,7], budget=19))  # 0
